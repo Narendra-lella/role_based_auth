b18036e38d80ab10c96137fceae42c401134609e
Narendra-Squadsync <narendra.squadsync@gmail.com>
2024-03-09 10:22:48 +0530

    version1

---
+++ accounts/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+
+from django.contrib.auth.models import AbstractUser
+from django.utils import timezone
+import uuid
+
+
+class Customuser(AbstractUser):
+    ADMIN = 'Admin'
+    MANAGER = 'Manager'
+    TEACHER = 'Teacher'
+    STUDENT = 'Student'
+    
+    ROLE_CHOICES =(
+        (ADMIN,'Admin'),
+        (MANAGER,'Manager'),
+        (TEACHER,'Teacher'),
+        (STUDENT,'Student')
+    )
+    uuid = models.UUIDField(unique=True,editable=False,default=uuid.uuid4)
+    username = models.CharField(max_length=50,blank=False,null=False)
+    email = models.EmailField(unique=True)
+    role = models.CharField(choices=ROLE_CHOICES,blank=True,null=True,max_length=50)
+    dob= models.CharField(max_length=20,null=True,blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    modified_date = models.DateTimeField(default=timezone.now)
+
+    USERNAME_FIELD = 'email'
+    REQUIRED_FIELDS = []
+
+    def __str__(self):
+        return self.username
+    
+    class Meta:
+
+        db_table = 'users_custom'
---
+++ accounts/serializers.py
@@ -0,0 +1,96 @@
+from django.contrib.auth.password_validation import validate_password
+from rest_framework import serializers
+
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.tokens import Token,RefreshToken,AccessToken
+from django.contrib.auth.models import update_last_login
+from rest_framework.validators import UniqueValidator
+from django.contrib.auth import authenticate
+
+from .models import Customuser
+
+class MyTokenObtainpairserializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token =  super().get_token(user)
+    
+        token['username'] = user.username
+        token['email'] = user.email 
+        token['role'] = user.role
+
+        return token
+    
+class Registrationserializer(serializers.ModelSerializer):
+    password = serializers.CharField(write_only=True,required=True,validators=[validate_password])
+    password2 = serializers.CharField(write_only = True,required=True)
+    email = serializers.EmailField(required=True,validators=[UniqueValidator(queryset=Customuser.objects.all())])
+
+    class Meta:
+        model = Customuser
+        fields = '__all__'
+    
+    def validate(self,attrs):
+        if attrs['password'] != attrs['password2']:
+            raise serializers.ValidationError(
+                {'password':"password Fields didn't matched"}
+            )
+        if attrs['username'] == None:
+            raise serializers.ValidationError(
+                {"username":"Username is required"}
+            )
+        return attrs
+    def create(self, validated_data):
+        user = Customuser.objects.create(
+            username = validated_data['username'],
+            email = validated_data['email'],
+            # dob=validated_data['dob'],
+            role = validated_data['role']
+        )
+
+        user.set_password(validated_data['password'])
+        user.save()
+
+        return user
+    
+class LoginSerializer(serializers.Serializer):
+    email = serializers.EmailField()
+    password = serializers.CharField(max_length=20,write_only=True)
+    access = serializers.CharField(read_only=True)
+    refresh = serializers.CharField(read_only=True)
+    role = serializers.CharField(read_only=True)
+
+    def validate(self,data):
+        email = data['email']
+        passowrd = data['password']
+        user = authenticate(email=email,password=passowrd)
+        if user is None:
+            raise serializers.ValidationError(
+                {"Invalied Credentials"}
+            )
+        try :
+            refresh = RefreshToken.for_user(user)
+            refresh_token = str(refresh)
+            access_token = str(refresh.access_token)
+
+            update_last_login(None,user)
+
+            validation = {
+                'access' : access_token,
+                'refresh' : refresh_token,
+                'email':user.email,
+                'username':user.username,
+                'role':user.role
+            }
+
+            return validation
+        except Customuser.DoesNotExist:
+            raise serializers.ValidationError({"invalied Credentials"})
+
+class UserListSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customuser
+        fileds = (
+            'email',
+            'username',
+            'role'
+        )
